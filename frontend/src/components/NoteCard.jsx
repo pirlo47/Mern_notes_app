d4a26b7604c33fc6637e60588a90291deb56de7b
pirlo47 <ntoahaep108@gmail.com>
2026-01-24 09:54:41 +0200

    feat: add delete functionality to NoteCard and handle empty notes state in HomePage

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -2,8 +2,25 @@ import { PenSquareIcon, Trash2Icon } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router'
 import { formatDate } from '../lib/utils';
+import toast from 'react-hot-toast';
+import api from '../lib/axios';
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note, setNotes }) => {
+
+    const handleDelete = async (e, id) => {
+        e.preventDefault(); // prevent default behaviour
+
+        if (!window.confirm("Are you sure you want to delete this note?")) return; 
+
+        try{
+            await api.delete(`/notes/${id}`);
+            toast.success("Note deleted successfully"); 
+            setNotes((prev) => prev.filter(note => note._id !== id)); //get rid of deleted note and update UI immediatly
+        } catch (error) {
+            console.log("error in handleDelete", error);
+            toast.error("Failed to delete note");
+        }
+    };
   return (
     <Link
         to={`/note/${note._id}`}
@@ -17,11 +34,10 @@ const NoteCard = ({ note }) => {
                 <span className="text-sm text-base-content/60">{formatDate(new Date(note.createdAt))}</span>
                 <div className="flex items-center gap-1">
                     <PenSquareIcon className="size-4" />
-                    <button className="btn btn-ghost btn-xs text-error">
+                    <button className="btn btn-ghost btn-xs text-error" onClick={(e) => handleDelete(e, note._id)}>
                         <Trash2Icon className="size-4" />
                     </button>
                 </div>
-
             </div>
         </div>
     </Link>

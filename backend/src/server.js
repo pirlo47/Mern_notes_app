83aa8d9c7be36a382d449127946db967e8e99376
pirlo47 <ntoahaep108@gmail.com>
2026-02-04 11:32:22 +0200

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import dotenv from "dotenv"
 import cors from "cors"
+import path from "path"
 
  
 import notesRoutes from "./routes/notesRoutes.js";
@@ -18,14 +19,18 @@ const app = express();
 
 const PORT = process.env.PORT || 5001; 
 
+const __dirname = path.resolve(); 
+
 
 
 //Middleware needed 
-app.use(
-    cors({
-        origin: "http://localhost:5173"
-    })
-)
+if(process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173"
+        })
+    );
+};
 app.use(express.json()); //this middleware will parse JSON bodies: req.body 
 
 
@@ -34,6 +39,15 @@ app.use(rateLimiter);
 
 // Use the route "/api/notes"
 app.use("/api/notes", notesRoutes);
+
+//use a different middleware for deployment 
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+if(process.env.NODE_ENV === "production") {
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 //Call the db.js to connect to the database 
 connectDB().then(() => {
